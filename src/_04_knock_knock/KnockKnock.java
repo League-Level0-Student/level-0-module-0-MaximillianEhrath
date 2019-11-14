5bce884e9689cf58c0ad02853b66989e2cd611fe
CalebVia <55600946+CalebVia@users.noreply.github.com>
2019-11-14 15:55:49 -0800

    added code for module0

---
+++ src/_04_knock_knock/KnockKnock.java
@@ -0,0 +1,18 @@
+package _04_knock_knock;
+
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"KnockKnock");
+		JOptionPane.showMessageDialog(null,"who's there");
+		JOptionPane.showMessageDialog(null,"Cody");
+		JOptionPane.showMessageDialog(null,"Cody who");
+		JOptionPane.showMessageDialog(null,"I am coding now");
+
+
+	}
+
+}
---
+++ src/_07_last_summer/LastSummer.java
@@ -0,0 +1,10 @@
+package _07_last_summer;
+
+public class LastSummer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
