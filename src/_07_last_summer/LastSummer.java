fd03a858eb7f7f4794c4fa59bcfa7e0e761b8b6c
CalebVia <55600946+CalebVia@users.noreply.github.com>
2019-11-14 15:57:22 -0800

    added code for module 0

--- src/_07_last_summer/LastSummer.java
+++ src/_07_last_summer/LastSummer.java
@@ -1,9 +1,12 @@
 package _07_last_summer;
 
+import javax.swing.JOptionPane;
+
 public class LastSummer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String input = JOptionPane.showInputDialog("What is your name?");
 
 	}
 
